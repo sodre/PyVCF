bfcedb9bad1a14074ac4526ffdb610611e073810
James Casbon <casbon@gmail.com>
2016-03-18 16:21:45 +0000

    Cut release.

--- vcf/__init__.py
+++ vcf/__init__.py
@@ -12,4 +12,4 @@ from vcf.filters import Base as Filter
 from vcf.parser import RESERVED_INFO, RESERVED_FORMAT
 from vcf.sample_filter import SampleFilter
 
-VERSION = '0.6.8.dev0'
+VERSION = '0.6.8'
